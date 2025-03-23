5e74f5f0c31ba43182e1003f537ec22f7710aa65
Tushar2254 <103147321+Tushar2254@users.noreply.github.com>
2025-03-23 12:11:36 +0530

    styling footer header changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component, Inject, inject, OnInit,PLATFORM_ID } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
 import AOS from 'aos';
 import { once } from 'node:events';
-import { RouterModule } from '@angular/router';
+import { Router,NavigationEnd, RouterModule } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,14 +11,30 @@ import { RouterModule } from '@angular/router';
 })
 export class AppComponent {
   title = 'Harmony';
-  constructor(@Inject(PLATFORM_ID)private platformId: object){}
-    ngOnInit(): void {
-      if (isPlatformBrowser(this.platformId)) { 
-        AOS.init({
-           once: true,
-        }); 
-        window.addEventListener('load', function() {
-          AOS.refresh();});
+
+  constructor(@Inject(PLATFORM_ID) private platformId: object, private router: Router) {}
+
+  ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      // Initialize AOS only in the browser
+      AOS.init({
+        once: true, // Run animation only once
+        duration: 1000,
+        easing: 'ease-in-out',
+      });
+
+      // Refresh AOS when the page has fully loaded
+      window.addEventListener('load', () => {
+        AOS.refresh();
+      });
+
+      // Reinitialize AOS on route change (important if you're navigating to different views)
+      this.router.events.subscribe((event: any) => {
+        if (event instanceof NavigationEnd) {
+          // Re-initialize AOS on route change to apply animations to the newly loaded content
+          AOS.refresh();
+        }
+      });
       }
     }
 }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -7,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-  
+  isMenuOpen: boolean = false;
+
+  // Method to toggle the state of the menu
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  // Listen for window resize event to reset menu state
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event): void {
+    // Close the menu when resizing to laptop or desktop view (above 768px)
+    if (window.innerWidth > 768) {
+      this.isMenuOpen = false;
+    }
+  }
 }
